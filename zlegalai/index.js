886cb616e64d91d4d33801447b7ac342cc7e7416
CodeWithPiyush <129826975+Piyushraj456@users.noreply.github.com>
2026-04-29 15:26:07 +0530

    Update index.js url

--- zlegalai/index.js
+++ zlegalai/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000","https://fyp-mu-lilac.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5000","https://legalease-ecru.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
