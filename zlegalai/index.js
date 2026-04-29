8336df83298f0c37126e0c0bba5845846127cbaf
CodeWithPiyush <129826975+Piyushraj456@users.noreply.github.com>
2026-04-29 06:14:34 +0530

    Updated frontend url

--- zlegalai/index.js
+++ zlegalai/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000","https://legalease-pi.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5000","https://leagalease.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
